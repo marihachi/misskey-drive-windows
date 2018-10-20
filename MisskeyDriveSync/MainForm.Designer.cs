03b3086ea79a82b242e15230905abdebc13e8227
marihachi <marihachi0620@gmail.com>
2018-10-20 22:38:05 +0900

    [wip]update form, add ref package misq

--- MisskeyDriveSync/MainForm.Designer.cs
+++ MisskeyDriveSync/MainForm.Designer.cs
@@ -28,31 +28,40 @@
 		/// </summary>
 		private void InitializeComponent()
 		{
-			this.tabControl1 = new System.Windows.Forms.TabControl();
+			this.mainTabControl = new System.Windows.Forms.TabControl();
 			this.tabPage1 = new System.Windows.Forms.TabPage();
 			this.tabPage2 = new System.Windows.Forms.TabPage();
 			this.tabPage3 = new System.Windows.Forms.TabPage();
-			this.tabControl1.SuspendLayout();
+			this.appNameLabel = new System.Windows.Forms.Label();
+			this.versionLabel = new System.Windows.Forms.Label();
+			this.cancelButton = new System.Windows.Forms.Button();
+			this.okButton = new System.Windows.Forms.Button();
+			this.applyButton = new System.Windows.Forms.Button();
+			this.mainTabControl.SuspendLayout();
+			this.tabPage3.SuspendLayout();
 			this.SuspendLayout();
 			// 
-			// tabControl1
+			// mainTabControl
 			// 
-			this.tabControl1.Controls.Add(this.tabPage1);
-			this.tabControl1.Controls.Add(this.tabPage2);
-			this.tabControl1.Controls.Add(this.tabPage3);
-			this.tabControl1.Location = new System.Drawing.Point(12, 12);
-			this.tabControl1.Name = "tabControl1";
-			this.tabControl1.Padding = new System.Drawing.Point(15, 3);
-			this.tabControl1.SelectedIndex = 0;
-			this.tabControl1.Size = new System.Drawing.Size(776, 426);
-			this.tabControl1.TabIndex = 0;
+			this.mainTabControl.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+			this.mainTabControl.Controls.Add(this.tabPage1);
+			this.mainTabControl.Controls.Add(this.tabPage2);
+			this.mainTabControl.Controls.Add(this.tabPage3);
+			this.mainTabControl.Location = new System.Drawing.Point(12, 12);
+			this.mainTabControl.Name = "mainTabControl";
+			this.mainTabControl.Padding = new System.Drawing.Point(15, 3);
+			this.mainTabControl.SelectedIndex = 0;
+			this.mainTabControl.Size = new System.Drawing.Size(600, 386);
+			this.mainTabControl.TabIndex = 0;
 			// 
 			// tabPage1
 			// 
 			this.tabPage1.Location = new System.Drawing.Point(4, 22);
 			this.tabPage1.Name = "tabPage1";
 			this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
-			this.tabPage1.Size = new System.Drawing.Size(768, 400);
+			this.tabPage1.Size = new System.Drawing.Size(592, 360);
 			this.tabPage1.TabIndex = 0;
 			this.tabPage1.Text = "全般";
 			this.tabPage1.UseVisualStyleBackColor = true;
@@ -62,40 +71,112 @@
 			this.tabPage2.Location = new System.Drawing.Point(4, 22);
 			this.tabPage2.Name = "tabPage2";
 			this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
-			this.tabPage2.Size = new System.Drawing.Size(768, 400);
+			this.tabPage2.Size = new System.Drawing.Size(592, 360);
 			this.tabPage2.TabIndex = 1;
 			this.tabPage2.Text = "アカウント";
 			this.tabPage2.UseVisualStyleBackColor = true;
 			// 
 			// tabPage3
 			// 
+			this.tabPage3.Controls.Add(this.versionLabel);
+			this.tabPage3.Controls.Add(this.appNameLabel);
 			this.tabPage3.Location = new System.Drawing.Point(4, 22);
 			this.tabPage3.Name = "tabPage3";
 			this.tabPage3.Padding = new System.Windows.Forms.Padding(3);
-			this.tabPage3.Size = new System.Drawing.Size(768, 400);
+			this.tabPage3.Size = new System.Drawing.Size(592, 360);
 			this.tabPage3.TabIndex = 2;
 			this.tabPage3.Text = "バージョン情報";
 			this.tabPage3.UseVisualStyleBackColor = true;
 			// 
+			// appNameLabel
+			// 
+			this.appNameLabel.AutoSize = true;
+			this.appNameLabel.Font = new System.Drawing.Font("MS UI Gothic", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+			this.appNameLabel.ForeColor = System.Drawing.SystemColors.HotTrack;
+			this.appNameLabel.Location = new System.Drawing.Point(29, 43);
+			this.appNameLabel.Name = "appNameLabel";
+			this.appNameLabel.Size = new System.Drawing.Size(339, 21);
+			this.appNameLabel.TabIndex = 0;
+			this.appNameLabel.Text = "Misskey ドライブ for Windows Desktop";
+			// 
+			// versionLabel
+			// 
+			this.versionLabel.AutoSize = true;
+			this.versionLabel.Font = new System.Drawing.Font("MS UI Gothic", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+			this.versionLabel.Location = new System.Drawing.Point(30, 102);
+			this.versionLabel.Name = "versionLabel";
+			this.versionLabel.Size = new System.Drawing.Size(71, 15);
+			this.versionLabel.TabIndex = 1;
+			this.versionLabel.Text = "Version: -";
+			// 
+			// cancelButton
+			// 
+			this.cancelButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+			this.cancelButton.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+			this.cancelButton.Location = new System.Drawing.Point(432, 404);
+			this.cancelButton.Name = "cancelButton";
+			this.cancelButton.Size = new System.Drawing.Size(85, 26);
+			this.cancelButton.TabIndex = 1;
+			this.cancelButton.Text = "キャンセル";
+			this.cancelButton.UseVisualStyleBackColor = true;
+			this.cancelButton.Click += new System.EventHandler(this.cancelButton_Click);
+			// 
+			// okButton
+			// 
+			this.okButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+			this.okButton.Location = new System.Drawing.Point(337, 404);
+			this.okButton.Name = "okButton";
+			this.okButton.Size = new System.Drawing.Size(85, 26);
+			this.okButton.TabIndex = 2;
+			this.okButton.Text = "OK";
+			this.okButton.UseVisualStyleBackColor = true;
+			this.okButton.Click += new System.EventHandler(this.okButton_Click);
+			// 
+			// applyButton
+			// 
+			this.applyButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+			this.applyButton.Location = new System.Drawing.Point(527, 404);
+			this.applyButton.Name = "applyButton";
+			this.applyButton.Size = new System.Drawing.Size(85, 26);
+			this.applyButton.TabIndex = 3;
+			this.applyButton.Text = "適用";
+			this.applyButton.UseVisualStyleBackColor = true;
+			this.applyButton.Click += new System.EventHandler(this.applyButton_Click);
+			// 
 			// MainForm
 			// 
+			this.AcceptButton = this.okButton;
 			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
 			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-			this.ClientSize = new System.Drawing.Size(800, 450);
-			this.Controls.Add(this.tabControl1);
+			this.CancelButton = this.cancelButton;
+			this.ClientSize = new System.Drawing.Size(624, 442);
+			this.Controls.Add(this.applyButton);
+			this.Controls.Add(this.okButton);
+			this.Controls.Add(this.cancelButton);
+			this.Controls.Add(this.mainTabControl);
+			this.MinimumSize = new System.Drawing.Size(640, 480);
 			this.Name = "MainForm";
 			this.Text = "Misskey ドライブ";
-			this.tabControl1.ResumeLayout(false);
+			this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.MainForm_FormClosing);
+			this.Load += new System.EventHandler(this.MainForm_Load);
+			this.mainTabControl.ResumeLayout(false);
+			this.tabPage3.ResumeLayout(false);
+			this.tabPage3.PerformLayout();
 			this.ResumeLayout(false);
 
 		}
 
 		#endregion
 
-		private System.Windows.Forms.TabControl tabControl1;
+		private System.Windows.Forms.TabControl mainTabControl;
 		private System.Windows.Forms.TabPage tabPage1;
 		private System.Windows.Forms.TabPage tabPage2;
 		private System.Windows.Forms.TabPage tabPage3;
+		private System.Windows.Forms.Label versionLabel;
+		private System.Windows.Forms.Label appNameLabel;
+		private System.Windows.Forms.Button cancelButton;
+		private System.Windows.Forms.Button okButton;
+		private System.Windows.Forms.Button applyButton;
 	}
 }
 
--- MisskeyDriveSync/MainForm.cs
+++ MisskeyDriveSync/MainForm.cs
@@ -16,5 +16,63 @@ namespace MisskeyDriveSync
 		{
 			InitializeComponent();
 		}
+
+		#region Refresh methods
+
+		private void refreshVersionInfo()
+		{
+			var assembly = System.Reflection.Assembly.GetExecutingAssembly();
+			var version = assembly.GetName().Version;
+
+			this.versionLabel.Text = $"Version: {version.Major}.{version.Minor}";
+		}
+
+		#endregion Refresh methods
+
+		#region Process Methods
+
+		private void processCannel()
+		{
+
+		}
+
+		private void processApply()
+		{
+			// TODO: save settings
+		}
+
+		#endregion Process Methods
+
+		private void MainForm_Load(object sender, EventArgs e)
+		{
+			refreshVersionInfo();
+		}
+
+		private void okButton_Click(object sender, EventArgs e)
+		{
+			processApply();
+			Hide();
+		}
+
+		private void applyButton_Click(object sender, EventArgs e)
+		{
+			processApply();
+		}
+
+		private void cancelButton_Click(object sender, EventArgs e)
+		{
+			processCannel();
+			Hide();
+		}
+
+		private void MainForm_FormClosing(object sender, FormClosingEventArgs e)
+		{
+			if (e.CloseReason == CloseReason.UserClosing)
+			{
+				e.Cancel = true;
+				processCannel();
+				Hide();
+			}
+		}
 	}
 }

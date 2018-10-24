b7ea20d6f614d4f60c1373b123e75efa4a85ea13
marihachi <marihachi0620@gmail.com>
2018-10-25 00:33:27 +0900

    fix indent

--- MisskeyDriveSync/Models/MisskeyDriveTreeNode.cs
+++ MisskeyDriveSync/Models/MisskeyDriveTreeNode.cs
@@ -3,16 +3,16 @@ using System.Collections.Generic;
 
 namespace MisskeyDriveSync.Models
 {
-    public class MisskeyDriveTreeNode
-    {
-        public bool IsRoot => (this.Parent == null);
+	public class MisskeyDriveTreeNode
+	{
+		public bool IsRoot => (this.Parent == null);
 
-        public MisskeyDriveTreeNode Parent { get; set; }
+		public MisskeyDriveTreeNode Parent { get; set; }
 
-        public Disboard.Misskey.Models.Folder Folder { get; set; }
+		public Disboard.Misskey.Models.Folder Folder { get; set; }
 
 		public List<Disboard.Misskey.Models.File> Files { get; set; } = new List<Disboard.Misskey.Models.File>();
 
 		public List<MisskeyDriveTreeNode> Children { get; set; } = new List<MisskeyDriveTreeNode>();
-    }
+	}
 }
